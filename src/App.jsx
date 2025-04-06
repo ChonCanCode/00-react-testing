6f130e40c80fdf78317ac09f4b6e327553645ef8
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-04-06 08:20:29 +0100

    20250406.01

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ export default function App () {
     <form className="new-item-form">
       <div className="form-row">
         <label>New Item</label>
-        
+        <input type="text" id="item" />
       </div>
+      <button className="btn">Add</button>
     </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+        <input type="checkbox" />
+          Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
   </>
 )
 }
