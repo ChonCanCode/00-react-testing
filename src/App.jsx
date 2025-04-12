ccbface9a664e0946f887807d2b2dbe9c00305e1
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-04-12 07:37:56 +0100

    20250412.1-adding delete function, code review next

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,26 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
@@ -35,12 +55,21 @@ export default function App() {
       <ul className="list">
         {todos.map((todo) => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
-                Item 1
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           );
         })}
